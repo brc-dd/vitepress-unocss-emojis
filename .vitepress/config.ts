5e024027cae87f68c41b761f05923bea889d9ba6
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-01-12 18:29:31 +0530

    fix

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -36,7 +36,42 @@ export default defineConfig({
             }
           })
         ]
-      })
+      }),
+      {
+        name: 'custom:adjust-order',
+        configResolved(c) {
+          movePlugin(
+            // @ts-expect-error ignore readonly type
+            c.plugins,
+            'vitepress',
+            'before',
+            'unocss:transformers:pre'
+          )
+        }
+      }
     ]
   }
 })
+
+function movePlugin(
+  plugins: { name: string }[],
+  pluginAName: string,
+  order: 'before' | 'after',
+  pluginBName: string
+) {
+  const pluginBIndex = plugins.findIndex((p) => p.name === pluginBName)
+  if (pluginBIndex === -1) return
+
+  const pluginAIndex = plugins.findIndex((p) => p.name === pluginAName)
+  if (pluginAIndex === -1) return
+
+  if (order === 'before' && pluginAIndex > pluginBIndex) {
+    const pluginA = plugins.splice(pluginAIndex, 1)[0]
+    plugins.splice(pluginBIndex, 0, pluginA)
+  }
+
+  if (order === 'after' && pluginAIndex < pluginBIndex) {
+    const pluginA = plugins.splice(pluginAIndex, 1)[0]
+    plugins.splice(pluginBIndex, 0, pluginA)
+  }
+}
